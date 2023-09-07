cc0d7d93de5ff300b9d9f87f0de6c1f4a21733aa
Abdo Asil <143807467+AElasil@users.noreply.github.com>
2023-09-07 17:49:13 +0300

    Add files via upload

---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="text-wrapper">Abdo Asil</div>
+          <div className="div">من انا</div>
+          <div className="text-wrapper-2">المشاريع</div>
+          <div className="text-wrapper-3">تواصل معي</div>
+          <div className="text-wrapper-4">الخدمات</div>
+          <p className="abdo-asil-UI-UX">اهلا بك انا Abdo Asil, انا مصمم&nbsp;&nbsp;UI/UX</p>
+          <div className="text-wrapper-5">Abdo Asil</div>
+          <div className="ellipse" />
+          <img className="element" alt="Element" src="/img/369884196-3516953438625103-2532781252923830686-n-1.png" />
+          <a href="https://www.facebook.com/AElasil" rel="noopener noreferrer" target="_blank">
+            <img className="frame" alt="Frame" src="/img/frame-4.svg" />
+          </a>
+          <a href="https://www.instagram.com/abdo_elasil/" rel="noopener noreferrer" target="_blank">
+            <img className="img" alt="Frame" src="/img/frame-3.svg" />
+          </a>
+          <img className="frame-2" alt="Frame" src="/img/frame-2.svg" />
+          <img className="frame-3" alt="Frame" src="/img/frame-1.svg" />
+          <a href="https://www.behance.net/AAsil" rel="noopener noreferrer" target="_blank">
+            <img className="frame-4" alt="Frame" src="/img/frame.svg" />
+          </a>
+          <div className="rectangle-2" />
+          <p className="p">حقوق النشر © Abdo Asil. كل الحقوق محفوظة</p>
+          <div className="text-wrapper-6">الخدمات</div>
+          <div className="text-wrapper-7">تصميم موقع</div>
+          <div className="text-wrapper-8">تصميم تطبيق</div>
+          <div className="text-wrapper-9">تصميمات سوشيال ميديا</div>
+          <div className="text-wrapper-10">Abdo Asil</div>
+          <div className="text-wrapper-11">خريطة الموقع</div>
+          <div className="text-wrapper-12">من انا</div>
+          <div className="text-wrapper-13">المشاريع</div>
+          <div className="text-wrapper-14">تواصل معي</div>
+          <div className="text-wrapper-15">الخدمات</div>
+          <div className="rectangle-3" />
+          <div className="group">
+            <div className="overlap-group">
+              <div className="rectangle-4" />
+              <div className="rectangle-5" />
+            </div>
+          </div>
+          <div className="text-wrapper-16">تسجيل</div>
+          <div className="text-wrapper-17">البريد الالكتروني</div>
+          <img className="arrow-pointing-left" alt="Arrow pointing left" src="/img/arrow-pointing-left.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
